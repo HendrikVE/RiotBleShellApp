50e8a98e05925121271dabec4ea597dc8aaefbda
HendrikVE <hendrik1995@msn.com>
2019-08-13 14:44:15 +0200

    migrate to androidx

--- app/src/main/java/de/vanappsteer/riotbleshell/activities/BleTerminalActivity.java
+++ app/src/main/java/de/vanappsteer/riotbleshell/activities/BleTerminalActivity.java
@@ -6,9 +6,9 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.method.ScrollingMovementMethod;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
