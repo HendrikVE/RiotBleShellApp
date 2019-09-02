01c0d87dd198359c30fc29227a6977bb83805ce2
HendrikVE <hendrik1995@msn.com>
2019-09-02 14:06:29 +0200

    add custom exception handler

--- app/src/main/java/de/vanappsteer/riotbleshell/RiotBleShellApp.java
+++ app/src/main/java/de/vanappsteer/riotbleshell/RiotBleShellApp.java
@@ -2,6 +2,29 @@ package de.vanappsteer.riotbleshell;
 
 import android.app.Application;
 
+import de.vanappsteer.riotbleshell.util.LoggingUtil;
+
 public class RiotBleShellApp extends Application {
+
+    private Thread.UncaughtExceptionHandler defaultUncaughtExceptionHandler;
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        defaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+
+        Thread.setDefaultUncaughtExceptionHandler((thread, ex) -> {
+
+            LoggingUtil.error(ex.getMessage());
+            ex.printStackTrace();
+
+            // re-throw critical exception further to the os (important)
+            defaultUncaughtExceptionHandler.uncaughtException(thread, ex);
+
+            System.exit(2);
+        });
+    }
 
 }
